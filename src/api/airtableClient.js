c569d9f1f467b0134a9984fc6e352f4eeb28d75a
Zvensson8 <128257651+Zvensson8@users.noreply.github.com>
2025-06-05 21:05:29 +0200

    Sanitize Airtable base ID

--- src/api/airtableClient.js
+++ src/api/airtableClient.js
@@ -4,9 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const { AIRTABLE_API_KEY, AIRTABLE_BASE_ID } = process.env;
+const sanitizedBaseId = (AIRTABLE_BASE_ID || '').replace(/\.$/, '');
 
 Airtable.configure({ apiKey: AIRTABLE_API_KEY });
 
-const base = Airtable.base(AIRTABLE_BASE_ID);
+const base = Airtable.base(sanitizedBaseId);
 
 export default base;
